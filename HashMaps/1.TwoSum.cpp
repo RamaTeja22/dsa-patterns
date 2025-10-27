5addc4d38b1aa78edafb9bb5f3db973db85a82c8
Rama Teja <2ramateja@gmail.com>
2025-10-27 08:50:56 +0530

    Create 1.TwoSum.cpp

---
+++ HashMaps/1.TwoSum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>res;
+        int n=nums.size();
+        unordered_map<int,int>hm;
+        int i;
+        for(i=0;i<n;i++){
+            if(hm.find(target-nums[i])!=hm.end()){
+                res.push_back(hm[target-nums[i]]);
+                res.push_back(i);
+                return res;
+            }
+            hm[nums[i]]=i;
+        }
+        return res;
+    }
+};
