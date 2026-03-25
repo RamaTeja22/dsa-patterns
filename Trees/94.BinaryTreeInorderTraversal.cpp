c430341ec421bed03b10de5c3153acf5509c3913
Rama Teja <2ramateja@gmail.com>
2026-03-25 21:31:30 +0530

    Update 94.BinaryTreeInorderTraversal.cpp

--- Trees/94.BinaryTreeInorderTraversal.cpp
+++ Trees/94.BinaryTreeInorderTraversal.cpp
@@ -24,4 +24,27 @@ public:
         inOrder(root,res);
         return res;
     }
+
+    // Iterative Stack
+    // go to left till null, then captures its value, explore right sub tree
+     vector<int> inorderTraversal(TreeNode* root){
+        stack<TreeNode*>st;
+        TreeNode* node=root;
+        vector<int>res;
+        while(true){
+            if(node!=NULL){
+                st.push(node);
+                node=node->left;
+            }
+            else{
+                if(st.empty())
+                break;
+                node=st.top();
+                st.pop();
+                res.push_back(node->val);
+                node=node->right;
+            }
+        }
+        return res;
+    }
 };
