66dafac163232525dc043caef529083168257b26
Rama Teja <2ramateja@gmail.com>
2026-04-27 17:23:36 +0530

    Update 53.MaximumSubArray.cpp

--- Arrays/53.MaximumSubArray.cpp
+++ Arrays/53.MaximumSubArray.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-      int n=nums.size();
-      int currSum=0,i;
-      int maxSum=INT_MIN;
-      for(i=0;i<n;i++){
-        currSum+=nums[i];
-        maxSum=max(maxSum,currSum);
-        if(currSum<0)
-        currSum=0;
-      }
-      return maxSum;
+        int n=nums.size();
+        int maxSum=nums[0];
+        int currentSum=0;
+        int i;
+        for(i=0;i<n;i++){
+            currentSum=max(nums[i],currentSum+nums[i]);
+            if(currentSum>maxSum){
+                maxSum=currentSum;
+            }
+            else if(currentSum<=0){
+                currentSum=0;
+            }
+        }
+        return maxSum;   
     }
 };
