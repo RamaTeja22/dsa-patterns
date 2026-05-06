24a220c0016cc1be16d579f21877ae1998e8a920
Rama Teja <2ramateja@gmail.com>
2026-05-07 00:10:28 +0530

    Update 31.NextPermutation.cpp

--- Arrays/31.NextPermutation.cpp
+++ Arrays/31.NextPermutation.cpp
@@ -1,3 +1,13 @@
+/*
+    1. find prefix match, iterate from right to see where lefter element less than its neighbour, to know at what position, we should swap
+    2. swap with whom? iterate from right to find the smmallest number greater than the one to be swapped
+    3. reverse the rest of the array
+    [2,1,5,4,3,0,0]
+    Step 1:- identify 1 as the element to build on next permutation
+    Step 2:- Identify 3 as the Swap partner for 1 => [2,3, 5,4,1,0,0]
+    Step 3:- Reverse to have in sorted order => [2,3,0,0,1,4,5]
+*/
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
