4312236c90b4e7c7aefb414be380278c576e800b
RamaTeja22 <2ramateja@gmail.com>
2026-05-09 00:13:12 +0530

    Update 1642.FurthestBuildingYouCanReach.cpp

--- Heaps/1642.FurthestBuildingYouCanReach.cpp
+++ Heaps/1642.FurthestBuildingYouCanReach.cpp
@@ -1,3 +1,8 @@
+/*
+Trick: Use a Max Heap to keep track of the largest jumps (height differences) that we have encountered so far. Always use bricks for the current jump, and if we run out of bricks, we can replace the largest jump with a ladder. This way, we can maximize the distance we can reach with the given number of bricks and ladders.
+Time Complexity: O(N log N) for the heap operations, where N is the number of buildings.
+Space Complexity: O(N) for the heap.
+*/
 class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
